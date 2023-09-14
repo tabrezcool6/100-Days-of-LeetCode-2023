02d38868fc3e4c4147f17686706715ea43a09d21
tabrezcool6 <tabrezcool6@gmail.com>
2023-09-14 22:57:37 +0530

    2. Add Two Numbers Solution

--- 2_add_two_numbers.java
+++ 2_add_two_numbers.java
@@ -1,14 +1,32 @@
 import java.util.*;
 
 class AddTwoNumbers{
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resultNode = new ListNode(0);
+        ListNode pointer = resultNode;
 
+        int carry = 0;
+
+        while(l1!=null || l2!=null){
+            int sum = 0 + carry;
+            if(l1!=null){
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2!=null){
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum/10;
+            sum = sum%10;
+            pointer.next = new ListNode(sum);
+            pointer = pointer.next;
+        }
+
+        if(carry == 1) pointer.next = new ListNode(carry);
+
+        return resultNode.next;
     }
-    
-    public static void main(String args[]){
-
-    }
-        
-    
-
 }
