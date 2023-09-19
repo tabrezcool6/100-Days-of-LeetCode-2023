c4dd701e4d30af8b8c761acff5591aa5c27bd36f
tabrezcool6 <tabrezcool6@gmail.com>
2023-09-19 22:47:49 +0530

    commit - solution 2

--- 2_add_two_numbers.java
+++ 2_add_two_numbers.java
@@ -1,36 +1,36 @@
-// // import java.util.*;
-// import java.util.ListNode;
+// import java.util.*;
+import java.util.ListNode;
 
-// class AddTwoNumbers{
+class AddTwoNumbers{
    
-//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//         ListNode resultNode = new ListNode(0);
-//         ListNode pointer = resultNode;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resultNode = new ListNode(0);
+        ListNode pointer = resultNode;
 
-//         int carry = 0;
+        int carry = 0;
 
-//         while(l1!=null || l2!=null){
-//             int sum = 0 + carry;
-//             if(l1!=null){
-//                 sum = sum + l1.val;
-//                 l1 = l1.next;
-//             }
+        while(l1!=null || l2!=null){
+            int sum = 0 + carry;
+            if(l1!=null){
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
 
-//             if(l2!=null){
-//                 sum = sum + l2.val;
-//                 l2 = l2.next;
-//             }
+            if(l2!=null){
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
 
-//             carry = sum/10;
-//             sum = sum%10;
-//             pointer.next = new ListNode(sum);
-//             pointer = pointer.next;
-//         }
+            carry = sum/10;
+            sum = sum%10;
+            pointer.next = new ListNode(sum);
+            pointer = pointer.next;
+        }
 
-//         if(carry == 1) pointer.next = new ListNode(carry);
+        if(carry == 1) pointer.next = new ListNode(carry);
 
-//         return resultNode.next;
-//     }
+        return resultNode.next;
+    }
     
 
-// }
+}
